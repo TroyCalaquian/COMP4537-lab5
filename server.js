9be3aa2f32a0631c09bb157db036332520eb6abe
LaggySoul <trcalaquian@gmail.com>
2024-03-09 20:54:21 -0800

    Rearranged to allow database connection

--- server.js
+++ server.js
@@ -11,87 +11,90 @@ const connection = mysql.createConnection({
   database: "Lab5",
 });
 
+connection.connect((err) => {
+  if (err) {
+    // TODO: Change to sending error and front facing string
+    console.log("Connection error message: " + err.message);
+    return;
+  }
+
+  // If connection is successful, execute the SQL statement to create the table
+  const createTableQuery = `
+        CREATE TABLE IF NOT EXISTS PATIENTS (
+          patientID INT AUTO_INCREMENT PRIMARY KEY,
+          name VARCHAR(255),
+          dob DATE
+        ) ENGINE=InnoDB;
+    `;
+
+  connection.query(createTableQuery, (err, result) => {
+    if (err) {
+      // If there's an error creating the table, send an error response to the client
+      res.end("Error: Unable to create the table");
+      console.log("Error creating table: " + err.message);
+      return;
+    }
+
+    // If table creation is successful, send a success response to the client
+    console.log("Table created successfully!");
+  });
+});
+
 // ChatGPT was used when creating this function
 http
   .createServer(function (req, res) {
-    connection.connect((err) => {
-      if (err) {
-        // TODO: Change to sending error and front facing string
-        console.log("Connection error message: " + err.message);
-        return;
-      }
+    if (req.url === "/lab5/api/v1/sql") {
+      if (req.method === GET) {
+        const statement = url.parse(req.url, true);
+        const query = statement.query["query"];
+        const decodedQuery = decodeURIComponent(query);
+        const splitQuery = decodedQuery.split(" ");
+        if (splitQuery[0].toLowerCase() === "select") {
+          connection.query(decodedQuery, (err, result) => {
+            if (err) {
+              res.end("Bad query");
+              return;
+            }
 
-      // If connection is successful, execute the SQL statement to create the table
-      const createTableQuery = `
-            CREATE TABLE IF NOT EXISTS PATIENTS (
-              patientID INT AUTO_INCREMENT PRIMARY KEY,
-              name VARCHAR(255),
-              dob DATE
-            ) ENGINE=InnoDB;
-        `;
-
-      connection.query(createTableQuery, (err, result) => {
-        if (err) {
-          // If there's an error creating the table, send an error response to the client
-          res.end("Error: Unable to create the table");
-          console.log("Error creating table: " + err.message);
-          return;
-        }
-
-        // If table creation is successful, send a success response to the client
-        console.log("Table created successfully!");
-      });
-
-      if (req.url === "/lab5/api/v1/sql") {
-        if (req.method === GET) {
-          const statement = url.parse(req.url, true);
-          const query = statement.query["query"];
-          const decodedQuery = decodeURIComponent(query);
-          const splitQuery = decodedQuery.split(" ");
-          if (splitQuery[0].toLowerCase() === "select") {
-            connection.query(decodedQuery, (err, result) => {
-              if (err) {
-                res.end("Bad query");
-                return;
-              }
-
-              res.writeHead(200, {
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "*",
-              });
-
-              const jsonResult = JSON.stringify({ results: result });
-              res.end(jsonResult);
-            });
-          } else {
-            res.writeHead(400, {
+            res.writeHead(200, {
               "Content-Type": "application/json",
               "Access-Control-Allow-Origin": "*",
               "Access-Control-Allow-Methods": "*",
             });
-            // TODO: Front facing string
-            res.end(
-              JSON.stringify({ error: "Only Select Queries are allowed" })
-            );
-          }
-        }
 
-        if (req.method === POST) {
-
+            const jsonResult = JSON.stringify({ results: result });
+            res.end(jsonResult);
+          });
+        } else {
+          res.writeHead(400, {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*",
+          });
+          // TODO: Front facing string
+          res.end(JSON.stringify({ error: "Only Select Queries are allowed" }));
         }
-      } else {
-        res.writeHead(404, {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "*",
-        });
-        res.end(JSON.stringify({ error: "Page not found!" }));
       }
-    });
-    connection.end();
+
+      if (req.method === POST) {
+      }
+    } else {
+      res.writeHead(404, {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+      });
+      res.end(JSON.stringify({ error: "Page not found!" }));
+    }
   })
   .listen(8888);
+
+// Close the database connection when the server is stopped
+process.on("SIGINT", () => {
+  connection.end();
+  console.log("Server stopped. Database connection closed.");
+  process.exit();
+});
 
 // CODE:
 // server {
